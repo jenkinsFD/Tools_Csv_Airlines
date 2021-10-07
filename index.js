9e63f5337d8afc76b2d4c82f61fea64dde81df64
jenkinsFD <91998827+jenkinsFD@users.noreply.github.com>
2021-10-06 22:02:22 -0500

    update numTest
    cambio variable dinamica desde jenkins

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ https://marak.github.io/faker.js/#toc7__anchor
     let ArrayMonth = ["01","02","03","04","05","06","07","08","09","10","11","12"];
     let ArrayYear = [1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027];
     let ArrayLevel = ["GOLD", "PLATINUM", "ZAFIR", "VIP", "BASIC", "JR"];
-    let numberTest = 10; //-Numero de registros a generar
     let nameFile = "./"+today+"_"+uui+"_Airlines_"+numberTest+".csv";  // construlle nombre del archivo
     let arrayData = [];
     for (var i = 0; i < numberTest; i++) {
